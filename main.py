26420973b5a315d35a8d28aa37b80d1bac04640b
Sammy Hansali <sammyhansali@gmail.com>
2025-10-19 15:38:30 -0400

    added basic logging setup

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
-import logging
+import json
+import pathlib
+import logging.config
 
+logger = logging.getLogger("sleep-analytics")
+
+def logging_setup():
+    logging_config = pathlib.Path("logging_config/basic.json")
+
+    with open(logging_config, "r") as f:
+        config = json.load(f)
+    logging.config.dictConfig(config)
+    
 def main():
-    logging.basicConfig(level=logging.INFO, format="%(asctime)s | %(levelname)s | %(message)s")
-    logger = logging.getLogger(__name__)
+    logging_setup()
 
     logger.info("Hello from sleep-analytics!")
 
